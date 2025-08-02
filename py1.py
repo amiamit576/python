6d56a9e1ba70f75bba6c31eff99fe1b897554c9d
amit kumar <amiamit576@gmail.com>
2025-08-02 19:47:11 +0530

    commit --initial commit

---
+++ py1.py
@@ -0,0 +1,43 @@
+# a="i study in Phsyics Wallah";
+
+# print(a[5]);
+# # to print  study
+# s=a[2:7];
+# print(s);
+# print(a[::2]);
+# print(a[::-1])
+# print("========================")
+# print(a[-5:-1:-1]);
+# name = "krish"
+# # age = 32
+# # print("my name is {} and age is {}".format(name, age))
+# name="krish Naik"
+# age=32;
+# degree="BE"
+
+
+# #  if else statement
+# age = int(input ("enter the age"))
+# if  age>=18 and age <=45:
+#     print("you are  young  blood");
+
+# # mall--->price less100 b20%price  gerater  than 1000 30%  discount and also  calculated  price  fter  discount
+# def get_discounted_price(price):
+#     if price<1000:
+#         discount_rate=0.2
+#     elif price>=1000:
+#         discount_rate=0.3
+#     discount = price * discount_rate
+#     final_price = price - discount
+
+#     return discount, final_price
+
+# print(get_discounted_price(1200));
+
+val=int(input("enter  the  number :"))
+if(val<=999):print("value is less  than  equal to 999")
+else:
+    print("go home")
+
+
+
